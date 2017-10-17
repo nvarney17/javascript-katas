35f4ce93ab14e5a708ef9922c197ae5ecdb69fdf
nvarney17 <nvarney@pathfinderstudents.org>
2017-10-17 11:00:21 -0400

    Update 2-while loop.js

--- 2-while loop.js
+++ 2-while loop.js
@@ -4,14 +4,22 @@
 // Prompt for flavor then output "heres your" +flavor
 // If strawberry, alert "all out of strawberry"
 // Else alert "no such flavor"
+// Continue while flavor not equal to "coffee"
+// Then write "No Coffee Allowed in School!"
 
-var flavor = prompt("Enter a flavor");
-if (flavor == "chocolate" || flavor == "vanilla") {
-	alert ("heres your "+flavor+" ice cream");
+do{
+	var flavor = prompt("Enter a flavor");
+	if (flavor == "chocolate" || flavor == "vanilla") {
+		alert ("heres your "+flavor+" ice cream");
+	}
+	else if (flavor == "strawberry") {
+		alert ("all out of "+flavor);
+	}
+	else if (flavor == "coffee") {
+	alert ("No Coffee Allowed In "+flavor+" School!");
+	}
+	else {
+		alert ("no such flavor");
+	}
 }
-else if (flavor == "strawberry") {
-	alert ("all out of "+flavor);
-}
-else {
-	alert ("no such flavor");
-}
+while(flavor != "coffee");
