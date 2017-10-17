5131d1c83ca8926ccb35654fb8b4a46e59903789
nvarney17 <nvarney@pathfinderstudents.org>
2017-10-17 10:57:20 -0400

    Add files via upload

---
+++ 2-while loop.js
@@ -0,0 +1,17 @@
+/* 1.1 - Conditionals with Prompt */
+
+// Conditional where flavor = chocolate or vanilla
+// Prompt for flavor then output "heres your" +flavor
+// If strawberry, alert "all out of strawberry"
+// Else alert "no such flavor"
+
+var flavor = prompt("Enter a flavor");
+if (flavor == "chocolate" || flavor == "vanilla") {
+	alert ("heres your "+flavor+" ice cream");
+}
+else if (flavor == "strawberry") {
+	alert ("all out of "+flavor);
+}
+else {
+	alert ("no such flavor");
+}
